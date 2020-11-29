17a2f21bf77abd455c44b6414ff1c6cdbe1dcf4e
“Sebastian_oviedo” <sebastianoviedo_2658@hotmail.com>
2020-11-28 19:02:52 -0500

    EvidenciasJs fin

--- js/juego.js
+++ js/juego.js
@@ -1,6 +1,7 @@
-var lanzarB = document.getElementById("boton");
+var lanzarB = document.getElementById("boton1");
 if (lanzarB) lanzarB.addEventListener("click",play,false);
-
+var boton2 = document.getElementById("boton2");
+if (boton2) boton2.addEventListener("click",reiniciar,false);
 var inicio = true;
 var punto = false;
 var val = 0;
@@ -8,35 +9,48 @@ var res = 0;
 var d1 = 0;
 var d2 = 0;
 var t2 = document.getElementById("txt");
+var td1 = document.getElementById("txtd1");
+var td2 = document.getElementById("txtd2");
 
 function play(){
-    if(inicio){
-        inicio=false;
-        d1=(math.floor(math.random()*100))%6+1;
-        d2=(math.floor(math.random()*100))%6+1;
-        val= d1 + d2;
-        document.getElementById("d1").src=(d1+".png");
-        document.getElementById("d2").src=(d2+".png");
-        if(val==2 || val==3 || val==12) t2.textContent="PERDISTE"
-        else if(val==7 || val==11) t2.textContent="GANASTE"
-        else {
-            punto=true;
-            t2.textContent="PERDISTE"+val;
-        }
+if(inicio){
+    inicio=false;
+    d1=(Math.floor(Math.random()*100))%6+1;
+    d2=(Math.floor(Math.random()*100))%6+1;
+    val= d1 + d2;
+    document.getElementById("d1").src=("../imgs/"+d1+".png");
+    document.getElementById("d2").src=("../imgs/"+d2+".png");
+    td1.textContent="Dado 1: "+d1;
+    td2.textContent="Dado 2: "+d2;
+    if(val==2 || val==3 || val==12) t2.textContent="PERDISTE";
+    else if(val==7 || val==11) t2.textContent="GANASTE";
+    else {
+        punto=true;
+        t2.textContent="PUNTO: "+val;
     }
-    else if(punto){
-        d1=(math.floor(math.random()*100))%6+1;
-        d2=(math.floor(math.random()*100))%6+1;
-        res= d1 + d2;
-        document.getElementById("d1").src=(d1+".png");
-        document.getElementById("d2").src=(d2+".png");
-        if(res=7) {
-            punto=false
-            t2.textContent="PERDISTE"
-        }
-        else if(res==val){
-            t2.textContent="GANASTE"
-            punto=false
-        } 
-    }  
+}
+
+else if(punto){
+    d1=(Math.floor(Math.random()*100))%6+1;
+    d2=(Math.floor(Math.random()*100))%6+1;
+    res= d1 + d2;
+    document.getElementById("d1").src=("../imgs/"+d1+".png");
+    document.getElementById("d2").src=("../imgs/"+d2+".png");
+    td1.textContent="Dado 1: "+d1;
+    td2.textContent="Dado 2: "+ d2;
+    t2.textContent="Vuelve y tira"; 
+    if(res==7) {
+        punto=false
+
+        t2.textContent="PERDISTE";
+    }
+    else if(res==val){
+        t2.textContent="GANASTE";
+        punto=false
+    } 
+}  
+}
+
+function reiniciar (){
+location.reload();
 }
